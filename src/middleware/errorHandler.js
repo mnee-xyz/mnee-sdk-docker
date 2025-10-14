a71e56a0ba6d9b2e1551cb14ccc6e8974120b498
sharath-mnee <skariyappa@mnee.io>
2025-10-14 13:34:34 +0530

    update: update errorhandler to include status.

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,6 @@
 export const errorHandler = (err, req, res, next) => {
     console.log(err)
-    const statusCode = err.statusCode || 500;
+    const statusCode = err.statusCode || err.status || 500;
     res.status(statusCode).json({
       success: false,
       message: err.message || 'Internal Server Error',
